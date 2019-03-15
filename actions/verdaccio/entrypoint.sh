ec95b814e1b2e55a1089eb98bd1737ca456d12e5
Ayush Sharma <ayush.aceit@gmail.com>
2019-03-15 20:53:33 +0100

    chore: add publish flow for local registry

--- actions/verdaccio/entrypoint.sh
+++ actions/verdaccio/entrypoint.sh
@@ -26,14 +26,4 @@ sh -c "nohup verdaccio --config $HOME/.config/verdaccio/config.yaml &>$tmp_regis
 sh -c "npm set registry https://registry.npmjs.org/"
 sh -c "npx npm-auth-to-token@1.0.0 -u test -p test -e test@test.local -r $local_registry"
 sh -c "npm whoami --registry $local_registry"
-
-if [ "$GITHUB_REF" = "master" ]; then
-  # RELEASE AS @NEXT
-	sh -c "yarn lerna publish --npm-client=npm --skip-git --exact --force-publish=* --canary=commit --yes --npm-tag=next --registry $local_registry"
-  exit 0
-else
-  # CANARY RELEASE WITH COMMIT NAME
-  # @commit
-  sh -c "yarn lerna publish --npm-client=npm --skip-git --exact --force-publish=* --canary=commit --yes --registry $local_registry"
-  exit 0
-fi
+sh -c "yarn lerna publish --yes --npm-client=npm --force-publish=* --skip-git --exact --npm-tag=latest --registry $local_registry"
