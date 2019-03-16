aa50e2cab533b370ac9c1bf160be2453d27a2c4b
Ayush Sharma <ayush.aceit@gmail.com>
2019-03-16 15:37:29 +0100

    chore: adds nvm action

--- actions/nvm/entrypoint.sh
+++ actions/nvm/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 set -e
-sh-c "wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash"
+
+sh -c "wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash"
 export NVM_DIR="${XDG_CONFIG_HOME/:-$HOME/.}nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 sh -c "nvm $*"
