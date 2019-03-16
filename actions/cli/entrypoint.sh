54368c271456422d3f37038a3a9c0ee097700519
Ayush Sharma <ayush.aceit@gmail.com>
2019-03-17 00:42:07 +0100

    chore: adds nvm action

--- actions/cli/entrypoint.sh
+++ actions/cli/entrypoint.sh
@@ -3,18 +3,20 @@
 set -e
 
 # THIS REQUIRES TO LOAD NVM
-exec bash
+# exec bash
 
-if [ -n "$NPM_AUTH_TOKEN" ]; then
-  # Respect NPM_CONFIG_USERCONFIG if it is provided, default to $HOME/.npmrc
-  NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG-"$HOME/.npmrc"}"
-  NPM_REGISTRY_URL="${NPM_REGISTRY_URL-registry.npmjs.org}"
+# if [ -n "$NPM_AUTH_TOKEN" ]; then
+#   # Respect NPM_CONFIG_USERCONFIG if it is provided, default to $HOME/.npmrc
+#   NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG-"$HOME/.npmrc"}"
+#   NPM_REGISTRY_URL="${NPM_REGISTRY_URL-registry.npmjs.org}"
 
-  # Allow registry.npmjs.org to be overridden with an environment variable
-  printf "//%s/:_authToken=%s\\nregistry=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "$NPM_REGISTRY_URL" > "$NPM_CONFIG_USERCONFIG"
-  chmod 0600 "$NPM_CONFIG_USERCONFIG"
-fi
+#   # Allow registry.npmjs.org to be overridden with an environment variable
+#   printf "//%s/:_authToken=%s\\nregistry=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "$NPM_REGISTRY_URL" > "$NPM_CONFIG_USERCONFIG"
+#   chmod 0600 "$NPM_CONFIG_USERCONFIG"
+# fi
+
+echo '"$@"' | bash
 
 for ARG in "$@"; do
-  sh -c "$ARG"
+  echo '"$ARG"'| bash
 done
