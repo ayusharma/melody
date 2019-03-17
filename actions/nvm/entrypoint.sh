144ea2f43d58f4626484d7b6ff3c5c99642396d5
Ayush Sharma <ayush.aceit@gmail.com>
2019-03-17 03:03:50 +0100

    chore: adds nvm action

--- actions/nvm/entrypoint.sh
+++ actions/nvm/entrypoint.sh
@@ -6,7 +6,7 @@ echo "$@"
 
 for ARG in "$@"; do
   if [[ $ARG == *"nvm"* ]]; then
-    sh -c  "source /root/.bashrc && $ARG"
+    sh -c  "source /root/.bashrc && $ARG | sed "s/^\([\"']\)\(.*\)\1\$/\2/g""
   else
     $ARG
   fi
