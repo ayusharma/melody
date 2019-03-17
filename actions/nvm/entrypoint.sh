6bb447d74e78f1a9807fb8f6968b89218b1d9146
Ayush Sharma <ayush.aceit@gmail.com>
2019-03-17 02:50:43 +0100

    chore: adds nvm action

--- actions/nvm/entrypoint.sh
+++ actions/nvm/entrypoint.sh
@@ -6,7 +6,7 @@ echo "$@"
 
 for ARG in "$@"; do
   if [[ $ARG == *"nvm"* ]]; then
-    sh -c  "source /usr/local/nvm/nvm.sh && $ARG"
+    sh -c  "source /usr/local/nvm/nvm.sh"
   else
     $ARG
   fi
