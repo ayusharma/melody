256a45a977c49db0ae2158c84d454bf5843a22a6
Ayush Sharma <ayush.aceit@gmail.com>
2019-11-04 23:41:23 +0100

    v1.2.0

--- actions/check-release-commit/main.js
+++ actions/check-release-commit/main.js
@@ -3,15 +3,15 @@
 const cp = require("child_process");
 cp.execSync(`cd ${__dirname}; npm ci`);
 
+const core = require("@actions/core");
+
 const exec = cmd => cp.execSync(cmd).toString().trim();
 
 const re = new RegExp(/^v(\d+\.){2}\d+(-\S+)?$/);
-const commitMessage = exec(`git log -1 --pretty=format:%s${process.env.GITHUB_SHA}}`);
+const commitMessage = exec(`git log -1 --pretty=format:%s${process.env.GITHUB_SHA}`);
 
 if (commitMessage.match(re) === null) {
-    console.error('Not a release commit');
-    process.exit(78);
+    core.setOutput("result", commitMessage);
 } else {
-    console.log('Release commit');
-    process.exit(0);
+    core.setOutput("result", "false");
 }
